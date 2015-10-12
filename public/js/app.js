108ea9ac1df036c574d592837fc958edfbb96851
bgveenstra <b.g.veenstra@gmail.com>
2015-10-12 01:26:48 -0700

    restructure client js to only re-render all for page load

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
 function pageLoad() {
   // load foods
-  getFoods();
+//  getFoods();
   // set event listeners
   $("#new-food-form").on("submit", function(e){
     // prevent form submission
@@ -23,34 +23,42 @@ function pageLoad() {
   });
 }
 
-function getFoods() {
-  $.get("/api/foods", function(response){ 
-    var foods = response.reverse();
-    // grab foods template
-    renderFoods(foods);
-  });
-}
+// function getFoods() {
+//   $.get("/api/foods", function(response){ 
+//     var foods = response.reverse();
+//     // grab foods template
+//     renderFoods(foods);
+//   });
+// }
 
-function renderFoods(foods) {
-  // clear content (for repeated use)
-  $("#food-ul").html("");
-  // loop over the foods and append to ul
-  for (var i=0; i<foods.length; i++) {
-    $("#food-ul").append("<li class='list-group-item'>" + foods[i].name + 
+// function renderFoods(foods) {
+//   // clear content (for repeated use)
+//   $("#food-ul").html("");
+//   // loop over the foods and append to ul
+//   for (var i=0; i<foods.length; i++) {
+//     $("#food-ul").append("<li class='list-group-item'>" + foods[i].name + 
+//       " <span class='label label-default'>"+foods[i].yumminess+"</span>" +
+//       "<button data-id="+foods[i].id+" onclick='deleteFood(this)' type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+//       "</li>");
+//   }
+// }
+
+function renderFood(food){
+  $("#food-ul").append("<li class='list-group-item'>" + foods[i].name + 
       " <span class='label label-default'>"+foods[i].yumminess+"</span>" +
       "<button data-id="+foods[i].id+" onclick='deleteFood(this)' type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
       "</li>");
-  }
 }
 
 function deleteFood(context) {
+  console.log(context);
   var foodId = $(context).data().id;
   $.ajax({
     url: '/api/foods/' + foodId,
     type: 'DELETE',
     success: function(response) {
-      // once successful, re-render all foods
-      getFoods();
+      // once successful, delete this food from view
+      $(context).closest('li').remove();
     }
   });
 }
